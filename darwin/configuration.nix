65ace750ac593bb1b00d2e9ab9bdf653fabd58b0
toonn <toonn@toonn.io>
2020-10-21 18:14:30 +0200

    Add haskell.nix binary caches

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -28,6 +28,11 @@
   # $ sysctl -n hw.ncpu
   nix.maxJobs = 8;
   nix.buildCores = 0;
+  nix.binaryCaches = [ "https://iohk.cachix.org" "https://hydra.iohk.io" ];
+  nix.binaryCachePublicKeys =
+    [ "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo="
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+    ];
 
 
   # Personal setup
