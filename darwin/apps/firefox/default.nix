017349d57fbb4a52b32ed7eb11facf0f6d0291ba
toonn <toonn@toonn.io>
2020-12-26 15:47:52 +0100

    Add macOS app expressions
    I use the upstream firefox app to avoid running a slower firefox due to
    the complicated build process.
    
    Nixpkgs doesn't have OpenEmu packaged.

---
+++ darwin/apps/firefox/default.nix
@@ -0,0 +1,39 @@
+{ stdenv, fetchurl, undmg }:
+stdenv.mkDerivation rec {
+  name = "firefox-app-${version}";
+
+  pname = "Firefox";
+
+  version = "latest";
+
+  # To update run:
+  # nix-prefetch-url --name 'firefox-app-latest.dmg' 'https://download.mozilla.org/?product=firefox-latest&os=osx&lang=en-US'
+  src = fetchurl {
+    url =
+      "https://download.mozilla.org/?product=firefox-latest&os=osx&lang=en-US";
+    name = "${name}.dmg";
+    sha256 = "159jkkyzhsq66c0fvq10yvaq03wahg8xxhhvrqj58r74qg4c93wy";
+  };
+
+  buildInputs = [ undmg ];
+
+  # The dmg contains the app and a symlink, the default unpackPhase tries to cd
+  # into the only directory produced so it fails.
+  sourceRoot = ".";
+
+  installPhase = ''
+    mkdir -p $out/Applications
+    mv ${pname}.app $out/Applications
+    '';
+
+  meta = with stdenv.lib; {
+    description = "Mozilla Firefox, free web browser (binary package)";
+    homepage = "http://www.mozilla.org/firefox/";
+    license = {
+      free = false;
+      url = "http://www.mozilla.org/en-US/foundation/trademarks/policy/";
+    };
+    maintainers = with maintainers; [ toonn ];
+    platforms = [ "x86_64-darwin" ];
+  };
+}
---
+++ darwin/apps/openemu/default.nix
@@ -0,0 +1,77 @@
+{ stdenv, fetchurl
+, unzip
+}:
+stdenv.mkDerivation rec {
+  name = "openemu-${version}";
+
+  version = "2.0.9.1";
+
+  src = fetchurl {
+    url =
+    "https://github.com/OpenEmu/OpenEmu/releases/download/v${version}/OpenEmu_${version}.zip";
+    name = name;
+    sha256 = "141phya7nivywfmdvcm1c1zkm2cf850yjbz13gpfz32f21s660y6";
+  };
+
+  buildInputs = [ unzip ];
+
+  unpackPhase = ''
+    unzip "$src"
+  '';
+
+  installPhase = ''
+    mkdir -p $out/Applications
+    mv OpenEmu.app $out/Applications
+  '';
+
+  meta = with stdenv.lib; {
+    description = "Retro video game emulation for macOS";
+    longDescription = ''
+      OpenEmu is an open source project whose purpose is to bring macOS game
+      emulation into the realm of first class citizenship. The project
+      leverages modern macOS technologies, such as Cocoa, Core Animation with
+      Quartz Composer, and other third-party libraries. One third-party library
+      example is Sparkle, which is used for auto-updating. OpenEmu uses a
+      modular architecture, allowing for game-engine plugins, allowing OpenEmu
+      to support a host of different emulation engines and back ends while
+      retaining the familiar macOS native front end.
+
+      Currently OpenEmu can load the following game engines as plugins:
+
+        - Atari 2600 (Stella)
+        - Atari 5200 (Atari800)
+        - Atari 7800 (ProSystem)
+        - Atari Lynx (Mednafen)
+        - ColecoVision (CrabEmu)
+        - Famicom Disk System (Nestopia)
+        - Game Boy / Game Boy Color (Gambatte)
+        - Game Boy Advance (mGBA)
+        - Game Gear (Genesis Plus)
+        - Intellivision (Bliss)
+        - NeoGeo Pocket (Mednafen)
+        - Nintendo (NES) / Famicom (FCEUX, Nestopia)
+        - Nintendo 64 (Mupen64Plus)
+        - Nintendo DS (DeSmuME)
+        - Odyssey² / Videopac+ (O2EM)
+        - PC-FX (Mednafen)
+        - SG-1000 (Genesis Plus)
+        - Sega 32X (picodrive)
+        - Sega CD / Mega CD (Genesis Plus)
+        - Sega Genesis / Mega Drive (Genesis Plus)
+        - Sega Master System (Genesis Plus)
+        - Sega Saturn (Mednafen)
+        - Sony PSP (PPSSPP)
+        - Sony PlayStation (Mednafen)
+        - Super Nintendo (SNES) (Higan, Snes9x)
+        - TurboGrafx-16 / PC Engine (Mednafen)
+        - TurboGrafx-CD / PCE-CD (Mednafen)
+        - Vectrex (VecXGL)
+        - Virtual Boy (Mednafen)
+        - WonderSwan (Mednafen)
+    '';
+    homepage = "https://openemu.org/";
+    license = licenses.unfree;
+    maintainers = with maintainers; [ toonn ];
+    platforms = [ "x86_64-darwin" ];
+  };
+}
