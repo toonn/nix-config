45418e5055740c7032b982089be3af7b4f1d5fa1
toonn <toonn@toonn.io>
2021-01-04 23:55:49 +0100

    haskell.nix: Bump haskell.nix pin

--- haskell.nix/nixpkgs.nix
+++ haskell.nix/nixpkgs.nix
@@ -1,7 +1,7 @@
 let
   haskellNix = import (builtins.fetchTarball
     ( "https://github.com/input-output-hk/haskell.nix/archive/"
-    + "d014079ccee72d9fbd05e4f0af8c6481034283de.tar.gz"
+    + "dff53790cc515f03a9013f222d0faf9761546315.tar.gz"
     )) {};
 in
   haskellNix.pkgs
