4c4bbca9fbbf4571f55a1ef0c353941397079d39
toonn <toonn@toonn.io>
2021-03-25 00:24:24 +0100

    Apps: Add Firefox 87.0
    Refactored the expression so I can keep all the versions. No good reason
    not to have the work in finding the hash and such carry over. Someone
    somewhere might need to install a previous version.

--- darwin/apps/firefox/default.nix
+++ darwin/apps/firefox/default.nix
@@ -8,11 +8,24 @@ stdenv.mkDerivation rec {
 
   # To update run:
   # nix-prefetch-url --name 'firefox-app-latest.dmg' 'https://download.mozilla.org/?product=firefox-latest&os=osx&lang=en-US'
-  src = fetchurl {
+  src = let
+    versions = {
+      "86.0" = {
+        sha256 = "04jslsfg073xb965hvbm7vdrdymkaiyyrgclv9qdpcyplis82rxc";
+      };
+      "86.0.1" = {
+        sha256 = "1cd55z11wpkgi1lnidwg8kdxy8b6p00arz07sizrbyiiqxzrmvx3";
+      };
+      "87.0" = {
+        sha256 = "1cih6i2p53mchqqrw2wlqhfka59p5qm4a7d0zc9ism0gvq5zpiz2";
+      };
+    };
+    latest = versions."87.0";
+  in fetchurl {
+    inherit (latest) sha256;
     url =
       "https://download.mozilla.org/?product=firefox-latest&os=osx&lang=en-US";
     name = "${name}.dmg";
-    sha256 = "04jslsfg073xb965hvbm7vdrdymkaiyyrgclv9qdpcyplis82rxc";
   };
 
   buildInputs = [ undmg ];
