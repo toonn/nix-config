83cabf1ec85cdf78f014766fccf88a2aaff30fca
toonn <toonn@toonn.io>
2021-10-29 13:31:20 +0200

    nixos: New machine, new stateVersion

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -109,15 +109,12 @@
   system.nssModules = with pkgs; [ nssmdns ];
   system.nssDatabases.hosts = [ "mdns4 [NOTFOUND=return]" ];
 
-  # Enable touchpad support.
-  services.xserver.libinput.enable = true;
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "21.05"; # Did you read the comment?
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.toonn = {
-    extraGroups = [ "wheel" ];
-    isNormalUser = true;
-    shell = pkgs.fish;
-  };
-
-  system.stateVersion = "19.03"; # ID10-t
 }
