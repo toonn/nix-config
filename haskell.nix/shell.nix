879a5a316e3a010e7497503dc10dbb417b7468bd
toonn <toonn@toonn.io>
2022-03-22 23:34:38 +0100

    haskell.nix: Bump tools for shell.nix

--- haskell.nix/shell.nix
+++ haskell.nix/shell.nix
@@ -6,24 +6,24 @@
 }:
 let
   haskell-nix = pkgs.haskell-nix;
-  stackage = haskell-nix.snapshots."lts-16.20";
+  stackage = haskell-nix.snapshots."lts-18.18";
   hackage-package = haskell-nix.hackage-package;
 in hsPkgs.shellFor {
   packages = ps: for;
 
   buildInputs =
     (map (p: stackage."${p}".components.exes."${p}")
-    [ "brittany"
+    [ # "brittany"
       "ghcid"
-      "ormolu"
+      # "ormolu"
     ]
     ) ++ buildInputs;
 
   exactDeps = true;
 
-  tools = let index-state = "2020-12-02T00:00:00Z";
-    in { cabal = { version = "3.2.0.0"; inherit index-state; };
-         fast-tags = { version = "2.0.0"; inherit index-state; };
+  tools = let index-state = "2021-11-30T00:00:00Z";
+    in { cabal = { version = "3.6.2.0"; inherit index-state; };
+         fast-tags = { version = "2.0.1"; inherit index-state; };
        };
 
   withHoogle = true;
