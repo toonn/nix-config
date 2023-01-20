b9fbf223f13cb7789388543d9f0e07ac6fb7dc6b
toonn <toonn@toonn.io>
2023-01-20 17:17:19 +0100

    Darwin: Update configuration to nixpkgs-22.11-darwin

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -12,7 +12,7 @@
   environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
 
   # Auto upgrade nix package and the daemon service.
-  # services.nix-daemon.enable = true;
+  services.nix-daemon.enable = true;
   nix.package = pkgs.nix;
 
   # Create /etc/bashrc that loads the nix-darwin environment.
@@ -26,10 +26,12 @@
 
   # You should generally set this to the total number of logical cores in your system.
   # $ sysctl -n hw.ncpu
-  nix.maxJobs = 8;
-  nix.buildCores = 0;
-  nix.binaryCaches = [ "https://iohk.cachix.org" "https://hydra.iohk.io" ];
-  nix.binaryCachePublicKeys =
+  nix.settings.max-jobs = 2;
+  nix.settings.cores = 1;
+  nix.settings.substituters = [ "https://iohk.cachix.org"
+                                "https://hydra.iohk.io"
+                              ];
+  nix.settings.trusted-public-keys =
     [ "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo="
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
     ];
@@ -69,14 +71,18 @@
         };
     };
   };
-  nixpkgs.overlays = [ (import ~/.config/nixpkgs/overlays/joypixels.nix)
+  nixpkgs.overlays = [ # (import ~/.config/nixpkgs/overlays/joypixels.nix)
                        (import ~/.config/nixpkgs/overlays/haskell-packages.nix)
+                       # (import ~/.config/nixpkgs/overlays/nix.nix)
                      ];
 
   #programs.tmux.enable = true;
   #programs.tmux.enableSensible = true;
 
   #programs.vim.enable = true;
+
+  # Doesn't work with old macOS
+  #services.karabiner-elements.enable = true;
   
   system.defaults = { NSGlobalDomain =
                         { AppleMeasurementUnits = "Centimeters";
@@ -93,7 +99,7 @@
                           NSNavPanelExpandedStateForSaveMode = true;
                           NSNavPanelExpandedStateForSaveMode2 = true;
                           NSTableViewDefaultSizeMode = 1;
-                          NSWindowResizeTime = "0.0001";
+                          NSWindowResizeTime = 0.0001;
                           PMPrintingExpandedStateForPrint = true;
                           PMPrintingExpandedStateForPrint2 = true;
                           "com.apple.mouse.tapBehavior" = 1;
@@ -102,8 +108,8 @@
                         };
                       LaunchServices.LSQuarantine = false;
                       dock = { autohide = true;
-                               autohide-delay = "0.15";
-                               autohide-time-modifier = "0.5";
+                               autohide-delay = 0.15;
+                               autohide-time-modifier = 0.5;
                                mineffect = "scale";
                                orientation = "left";
                                show-recents = false;
@@ -125,7 +131,7 @@
                       remapCapsLockToEscape = true;
                     };
 
-  fonts = { enableFontDir = true;
+  fonts = { fontDir.enable = true;
             fonts = with pkgs; [ dejavu_fonts
                                  #emojione  # Doesn't build
                                  #symbola  # Not foss anymore
@@ -140,5 +146,6 @@
 
   # Otherwise hm Applications don't end up in .nix-profile
   home-manager.useUserPackages = false;
+  # home-manager.useGlobalPackages = true; # Doesn't exist yet?
   home-manager.users.toonn = import ~/.config/nixpkgs/home.nix;
 }
