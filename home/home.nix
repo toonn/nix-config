a505b0b5881269eabb300c2409e9bbd92dedd85e
toonn <toonn@toonn.io>
2023-08-24 15:51:54 +0200

    home: Add Xresources config to make XTerm more pleasant

--- home/home.nix
+++ home/home.nix
@@ -175,6 +175,76 @@
   # Caveat Emptor: Changing stateVersion may require manual data conversion or
   #                moving of files.
   home.stateVersion = "22.11";
+  xresources.properties = {
+    "*faceName" = "DejaVu Sans Mono";
+    "*faceSize" = 10;
+    "*termName" = "xterm-256color";
+    "*ttyModes" = "erase ^?";
+    "*VT100.backarrowkey" = false;
+
+    # Colorscheme based on accessiblepalette.com
+    # set colors '#000000' '#fa4c35' '#27da3c' '#fecf49' '#3991cf' '#ff57ec' '#1ed1d1' '#dddddd' '#777777' '#ffa690' '#91e987' '#ffecba' '#75aadb' '#ff93f4' '#13ecec' '#ffffff'; for f in (seq -f %02g 0 15); for b in (seq -f %02g 0 15); hex $colors[(math $f + 1)] $colors[(math $b + 1)] "$f$b"; end; printf (tput sgr0)'\n'; end # Accessible base
+    # Shortcomings: dark yellow, dark cyan, light magenta, contrast green on
+    #               red
+    "*VT100.color0"  = "#000000";
+    "*VT100.color1"  = "#fa4c35";
+    "*VT100.color2"  = "#27da3c";
+    "*VT100.color3"  = "#fecf49";
+    "*VT100.color4"  = "#3991cf";
+    "*VT100.color5"  = "#ff57ec";
+    "*VT100.color6"  = "#1ed1d1";
+    "*VT100.color7"  = "#dddddd";
+    "*VT100.color8"  = "#777777";
+    "*VT100.color9"  = "#ffa690";
+    "*VT100.color10" = "#91e987";
+    "*VT100.color11" = "#ffef77"; # "#ffecba";
+    "*VT100.color12" = "#75aadb";
+    "*VT100.color13" = "#ff93f4";
+    "*VT100.color14" = "#13ecec";
+    "*VT100.color15" = "#ffffff";
+
+    # # My Kitty colorscheme
+    # "*VT100.color0"  = "#404040";
+    # "*VT100.color8"  = "#666666";
+    # #: black
+
+    # "*VT100.color1"  = "#b75757";
+    # "*VT100.color9"  = "#ce8d8d";
+    # #: red
+
+    # "*VT100.color2"  = "#87b757";
+    # "*VT100.color10" = "#adce8d";
+    # #: green
+
+    # "*VT100.color3"  = "#b7b757";
+    # "*VT100.color11" = "#cece8d";
+    # #: yellow
+
+    # "*VT100.color4"  = "#5777b7";
+    # "*VT100.color12" = "#8da3ce";
+    # #: blue
+
+    # "*VT100.color5"  = "#b757b7";
+    # "*VT100.color13" = "#ce8dce";
+    # #: magenta
+
+    # "*VT100.color6"  = "#57b7b7";
+    # "*VT100.color14" = "#8dcece";
+    # #: cyan
+
+    # "*VT100.color7"  = "#dddddd";
+    # "*VT100.color15" = "#ffffff";
+    # #: white
+
+    "*VT100.metaSendsEscape" = true;
+    "*VT100*translations" = ''#override \n\
+      Ctrl <Key>-: smaller-vt-font() \n\
+      Ctrl <Key>+: larger-vt-font() \n\
+      Ctrl Shift <Key>C: copy-selection(CLIPBOARD) \n\
+      Ctrl Shift <Key>V: insert-selection(CLIPBOARD) \n\
+      Alt <Key>I: set-reverse-video(toggle) \n\
+    '';
+  };
 
   programs.direnv = { enable = true;
                       enableFishIntegration = true;
