7f0322bdc1b0c213c5ab4a9421c70c3291386aff
toonn <toonn@toonn.io>
2024-04-16 17:08:26 +0200

    overlays/haskell-packages: Drop arbtt

--- overlays/haskell-packages.nix
+++ overlays/haskell-packages.nix
@@ -1,9 +1,5 @@
 self: super:
-{ arbtt = super.buildEnv {
-    name = "arbtt";
-    paths = super.lib.attrsets.attrValues (import ~/opt/arbtt/default.nix {});
-  };
-  bfpt = (import ~/src/bfpt/default.nix {}).bfpt.components.exes.bfpt;
+{ bfpt = (import ~/src/bfpt/default.nix {}).bfpt.components.exes.bfpt;
   coldasdice = (import
     ~/src/coldasdice/default.nix
     {}).coldasdice.components.exes.cadice;
