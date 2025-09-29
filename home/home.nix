9b3d2e45f98ffa2b8789869b85a3537a3ae62aea
toonn <toonn@toonn.io>
2025-09-29 14:53:53 +0200

    home: Add autofirma

--- home/home.nix
+++ home/home.nix
@@ -1,4 +1,14 @@
 { config, pkgs, lib, ... }:
+let
+  autofirma-flake = (import
+    ( fetchTarball {
+        url = "https://github.com/edolstra/flake-compat/archive/v1.1.0.tar.gz";
+        sha256 = "sha256:19d2z6xsvpxm184m41qrpi1bplilwipgnzv9jy17fgw421785q1m";
+      }
+    )
+    { src =  <autofirma-nix>; }
+  ).defaultNix;
+ in
 { nixpkgs = {
     config = {
       allowUnfreePredicate = p: builtins.elem (lib.getName p) [
@@ -29,6 +39,10 @@
       (import /home/toonn/src/nix-config/overlays/taskell.nix)
     ];
   };
+
+  imports = [
+    autofirma-flake.homeManagerModules.default
+  ];
 
   home.username = "toonn";
 
@@ -272,6 +286,11 @@
       Ctrl Shift <Key>V: insert-selection(CLIPBOARD) \n\
       Alt <Key>I: set-reverse-video(toggle) \n\
     '';
+
+  programs.autofirma = {
+    # config.checkForUpdates = false;
+    enable = true;
+    firefoxIntegration.profiles.cmyk.enable = true;
   };
 
   programs.direnv = { enable = true;
